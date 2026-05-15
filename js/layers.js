d824013152dc5dfc9b620c2e2f5f50b824a4fb90
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-15 14:30:48 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,13 +37,13 @@ addLayer("s", {
             points: new Decimal(0),
         }
     },
-    color: "#4BDC13",
+    color: "#1BAC00",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "stige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() { return player.points }, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.3, // Prestige currency exponent
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
