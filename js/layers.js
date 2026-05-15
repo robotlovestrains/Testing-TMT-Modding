f1c7a522838f6d7b230458fb88116ba0ceb6e86b
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-15 15:56:48 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -55,6 +55,17 @@ addLayer("p", {
             cost: new Decimal(15),
         },
     },
+    buyables: {
+    11: {
+        cost(x) { return new Decimal(1).mul(x) },
+        display() { return "Blah" },
+        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        buy() {
+            player[this.layer].points = player[this.layer].points.sub(this.cost())
+            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+        },
+    },
+    },
     layerShown() { return true }
 })
 
