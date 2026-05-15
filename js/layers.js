641a7b1e79c911a68641b9b53bed05ad76b76f20
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-15 15:46:24 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -59,45 +59,7 @@ addLayer("p", {
         },
     },
     buyables: {
-        11: {
-            cost(x) { return new Decimal(1.5).mul(x) },
-            display() { return "First Is The Worst" },
-            canAfford() { return player[this.layer].points.gte(this.cost()) },
-            buy() {
-                player[this.layer].points = player[this.layer].points.sub(this.cost())
-                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(20))
-            },
-            effect() {
-                return player[this.layer].this.id.add(1).pow(0.1)
-            },
-            unlocked() {false}
-        },
-        12: {
-            cost(x) { return new Decimal(1.5).mul(x) },
-            display() { return "Second Is The Best" },
-            canAfford() { return player[this.layer].points.gte(this.cost()) },
-            buy() {
-                player[this.layer].points = player[this.layer].points.sub(this.cost())
-                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(50))
-            },
-            effect() {
-                return player[this.layer].this.id.add(1)
-            },
-            unlocked() {false}
-        },
-        13: {
-            cost(x) { return new Decimal(1.5).mul(x) },
-            display() { return "Third Has The Tressure Chest" },
-            canAfford() { return player[this.layer].points.gte(this.cost()) },
-            buy() {
-                player[this.layer].points = player[this.layer].points.sub(this.cost())
-                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(50))
-            },
-            effect() {
-                return player[this.layer].this.id.add(1).pow(1.5)
-            },
-            unlocked() {false}
-        },
+        
     },
     layerShown() { return true }
 })
