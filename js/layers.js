2a78a2a61ca9b25ab1850afb083c8874b9ef3826
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-16 06:42:54 -0500

    Update layers.js
    Updates and Updates

--- js/layers.js
+++ js/layers.js
@@ -38,7 +38,7 @@ addLayer("p", {
             effect() {
                 return player[this.layer].points.add(1).pow(0.5)
             },
-            effectDisplay() { return "x" + format(upgradeEffect(this.layer, this.id)) }, // Add formatting to the effect
+            effectDisplay() { return "x" + format(upgradeEffect(this.layer, this.id)) },
         },
         13: {
             title: "And Thrird Comes Later",
@@ -47,7 +47,7 @@ addLayer("p", {
             effect() {
                 return player.points.add(1).pow(0.15)
             },
-            effectDisplay() { return "x" + format(upgradeEffect(this.layer, this.id)) }, // Add formatting to the effect
+            effectDisplay() { return "x" + format(upgradeEffect(this.layer, this.id)) },
         },
         14: {
             title: "And Last Comes Last",
@@ -55,13 +55,28 @@ addLayer("p", {
             cost: new Decimal(15),
         },
     },
-    layerShown() { return true }
+    buyables: {
+        11: {
+            title = "First is the Worst",
+            cost(x) { return new Decimal(1).mul(x) },
+            display() { return "Boost Points by x" + format(upgradeEffect(this.layer, this.id)) },
+            canAfford() { return player[this.layer].points.gte(this.cost()) },
+            buy() {
+                player[this.layer].points = player[this.layer].points.sub(this.cost())
+                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+            },
+            effect() {
+                return this.getBuyableAmount(this.layer, this.id).mul(0.25).add(1)
+            },
+        },
+    },
+    layerShown() { return true },
 })
 
 addLayer("s", {
-    name: "stige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    name: "stige",
+    symbol: "S",
+    position: 1,
     startData() {
         return {
             unlocked: true,
@@ -69,20 +84,20 @@ addLayer("s", {
         }
     },
     color: "#1BAC00",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "stige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() { return player.points }, // Get the current amount of baseResource
-    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.3, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+    requires: new Decimal(200),
+    resource: "stige points",
+    baseResource: "points",
+    baseAmount() { return player.points },
+    type: "static",
+    exponent: 0.3,
+    gainMult() {
         mult = new Decimal(1)
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 0,
     hotkeys: [
         { key: "s", description: "S: Reset for Stige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
     ],
