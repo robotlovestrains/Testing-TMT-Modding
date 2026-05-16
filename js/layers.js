8bf62f6be96d345e3070002c6a8b913de36473c6
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-16 06:51:41 -0500

    Update layers.js
    Fixes

--- js/layers.js
+++ js/layers.js
@@ -59,14 +59,14 @@ addLayer("p", {
         11: {
             title = "First is the Worst",
             cost(x) { return new Decimal(1).mul(x) },
+            effect() {
+                return this.getBuyableAmount(this.layer, this.id).mul(0.25).add(1)
+            },
             display() { return "Boost Points by x" + format(upgradeEffect(this.layer, this.id)) },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             buy() {
                 player[this.layer].points = player[this.layer].points.sub(this.cost())
                 setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
-            },
-            effect() {
-                return this.getBuyableAmount(this.layer, this.id).mul(0.25).add(1)
             },
         },
     },
