522c825c665e58a6dea22b34339524274d134ffc
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-05-28 11:07:08 -0500

    so many bugs

--- js/mod.js
+++ js/mod.js
@@ -58,7 +58,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.finish.gte(new Decimal("1"))
+	return player.finish.points.gte(new Decimal("1"))
 }
 
 
