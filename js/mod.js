4950c4cf3adc848fc9a25bd97f47f286247b72a8
robotlovestrains <138071259+robotlovestrains@users.noreply.github.com>
2026-06-26 14:40:59 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -63,7 +63,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("10000000"))
+	return player.points.gte(new Decimal("e11"))
 }
 
 
